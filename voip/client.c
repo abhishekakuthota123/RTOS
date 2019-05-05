b8c33845bd77b74c3017228d5fcfea51c4847395
abhishekakuthota123 <abhishekakuthota@gmail.com>
2019-05-05 18:28:09 +0530

    Add files via upload

---
+++ voip/client.c
@@ -0,0 +1,74 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <arpa/inet.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+
+#define BUFSIZE 1024
+
+int main(int argc, char*argv[]){
+	int client_fd, len_c, n, check;
+	struct sockaddr_in server;
+    uint8_t buf[BUFSIZE];
+    int con_check;
+    unsigned char *message;
+
+    static const pa_sample_spec ss = {
+        .format = PA_SAMPLE_S16LE,
+        .rate = 41000,
+        .channels = 2
+    };
+    pa_simple *s = NULL;
+    int ret = 1;
+    int error;
+
+    client_fd = socket(AF_INET,SOCK_STREAM,0);
+
+    server.sin_family=AF_INET;
+	server.sin_addr.s_addr=inet_addr(argv[1]);
+	server.sin_port=htons(8000);
+	memset(server.sin_zero, '\0', sizeof server.sin_zero);
+
+	if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        goto finish;
+    }
+
+	con_check = connect(client_fd,(struct sockaddr *)&server, sizeof(server));
+
+    while(1){
+    	
+		if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
+            fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+            goto finish;
+        }
+        /*
+        message = (unsigned char*)buf;
+		write(client_fd,message,BUFSIZE);
+		*/
+		write(client_fd,buf,sizeof(buf));
+
+	}
+	ret = 0;
+
+finish:
+
+    if (s)
+        pa_simple_free(s);
+
+    return ret;
+}
